ffb00a8320cb3b727040f38834cdf72a791612c1
Krzysztof Jurkowski <krzysztof.jurkowski@tm1.edu.pl>
2025-11-17 15:10:49 +0000

    Formatting
    Signed-off-by: GitHub <noreply@github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,14 @@
-import { ThemeProvider } from '@/components/theme'
-import type { Metadata } from 'next'
-import { NextIntlClientProvider } from 'next-intl'
-import {GeistSans, GeistMono} from "geist/font/nextjs';
-import './globals.css'
+import { ThemeProvider } from '@/components/theme';
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
+import type { Metadata } from 'next';
+import { NextIntlClientProvider } from 'next-intl';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'eDziennik',
   description: 'An open-source electronic gradebook for schools.',
 };
-
-
 
 export default async function RootLayout({
   children,
@@ -17,7 +16,11 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={`${GeistSans.variable} ${GeistMono.variable}`}>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${GeistSans.variable} ${GeistMono.variable}`}
+    >
       <head />
       <body>
         <ThemeProvider
