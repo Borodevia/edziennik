b5c871797f92f7ce7e70f6464f71880298b54046
Milosz Karpeta <82086059+Milosz-20@users.noreply.github.com>
2025-11-18 13:24:05 +0100

    Refactor layout styles in Home component and format tsconfig.json for… (#43)
    Refactor layout styles in Home component and format tsconfig.json for better readability

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import { mockScheduleData } from '@/data/mock-schedule';
 export default function Home() {
   const todayDate = new Date();
   return (
-    <div className="flex flex-col sm:grid sm:grid-cols-[minmax(0,1fr)_auto] sm:grid-rows-2 h-[calc(100vh-4rem)] max-h-[calc(100vh-4rem)] p-4 gap-4 overflow-hidden">
+    <div className="flex flex-col sm:grid sm:grid-cols-[minmax(0,1fr)_auto] sm:grid-rows-2 h-svh max-h-svh p-2 gap-4 overflow-hidden">
       <GradesCard />
       <ScheduleCard scheduleData={mockScheduleData} todayDate={todayDate} />
       <ExamsCard />
